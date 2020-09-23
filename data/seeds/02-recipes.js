d0d26f1cd1b7705db4d3d0d2f57a6d12ec7d3257
Ryan Schueck <ryschueck@gmail.com>
2020-09-23 16:22:11 -0400

    took out user_id in seeds

--- data/seeds/02-recipes.js
+++ data/seeds/02-recipes.js
@@ -4,21 +4,19 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex('recipes').insert([
         {
-          user_id: 1,
           title: "lemon-salmon",
           source: "Joe bob",
           ingredients: "Salmon, lemon slics,",
           instructions: "wrap Salmon and lemon slices in tin foil. cook in oven at 350",
-          category: "dinner"
+          category: "dinner",
         },
 
         {
-          user_id: 2,
           title: "scrambled eggs",
           source: "Justin",
           ingredients: "Eggs, cheese, salt, pepper",
           instructions: "scramble the eggs, cook on med heat. add cheese, and salt to peper per taste",
-          category: "breakfast"
+          category: "breakfast",
         }
 
 
